e9ba9d6f65e427364fb0473d0a245133624a5dc7
michael <michael@shoshi.dog>
2026-06-14 04:10:17 +0300

    refactor db: Inbox extends Set with static identity/of

--- packages/db/src/inbox.js
+++ packages/db/src/inbox.js
@@ -1,15 +1,24 @@
-export function createMemoryInbox() {
-    const seen = new Set
+export default class Inbox extends Set {
+    name = 'Inbox'
 
-    return {
-        has(eid) {
-            return seen.has(eid)
-        },
-
-        mark(eid) {
-            seen.add(eid)
-
-            return true
-        },
+    mark(id) {
+        this.add(id)
+        return true
     }
+
+    get [ Symbol.toStringTag ]() {
+        return this.name
+    }
+
+    static identity(msg) {
+        return msg?.eid ?? msg?.cmd ?? void 0
+    }
+
+    static of() {
+        return Reflect.construct(this, arguments)
+    }
+}
+
+export function createMemoryInbox() {
+    return new Inbox
 }
