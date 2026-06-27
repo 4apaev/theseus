220e9ae9be28bad5cdf8c88de1e299dbe268ebbb
michael <michael@shoshi.dog>
2026-06-28 02:08:28 +0300

    update deps & test script can recive arguments

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+if   [[ "$1" == "int"* ]]; then SPECS=test/*.integration.spec.js # starts with `int`
+elif [[ -n "$1" ]];        then SPECS=test/*.$1.spec.js          # not empty
+else                            SPECS=test/*.spec.js             # all
+fi
+
+echo $SPECS
+
 node                             \
     --env-file=./.env            \
     --experimental-test-coverage \
     --test-coverage-lines=80     \
     --test-coverage-branches=80  \
     --test-coverage-functions=80 \
-    --test                       \
-    ./test/*.spec.js
+    --test $SPECS
+
