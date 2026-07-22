3be43456286567b4e5ffe66cebeb9c775be3ed8d
michael <michael@shoshi.dog>
2026-07-22 05:00:29 +0300

    infra: check kafka-ui in infra-health

--- scripts/infra-health.js
+++ scripts/infra-health.js
@@ -6,7 +6,7 @@ const timeout = readEnv('HEALTH_TIMEOUT', 1500)
 const checks = await Promise.all([
     check('kafka'   , readEnv('KAFKA_HOST'   , '127.0.0.1'), readEnv('KAFKA_PORT'   , 9092)),
     check('postgres', readEnv('PG_HOST'      , '127.0.0.1'), readEnv('PG_PORT'      , 5432)),
-    // check('kafka-ui', readEnv('KAFKA_UI_HOST', '127.0.0.1'), readEnv('KAFKA_UI_PORT', 8080)),
+    check('kafka-ui', readEnv('KAFKA_UI_HOST', '127.0.0.1'), readEnv('KAFKA_UI_PORT', 8080)),
 ])
 
 for (let x of checks) {
