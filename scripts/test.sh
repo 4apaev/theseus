547823e50396c6c66f06f89351662f276c46a38a
4apaev <4apaev@gmail.com>
2026-09-11 04:02:42 +0300

    scripts: test - optional coverage. coverage for integration is up to 80%

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,25 +6,32 @@ shopt -s extglob
 CONCURRENCY=""
 GLOBAL_SETUP=""
 COVERAGE=${2:-"90"}
+COVER="--experimental-test-coverage"
 
 if [[ "$1" == "int"* ]]; then
     SPECS="test/*.int*.spec.js"
-    COVERAGE=50
+    COVERAGE=80
     CONCURRENCY="--test-concurrency=1"
     GLOBAL_SETUP="--test-global-setup=./scripts/reset-test-db.js"
 elif [[ -n "$1" ]]; then
-    SPECS="test/*.$1.spec.js"
+    SPECS="test/$1.spec.js"
+    COVER=""
 else
     SPECS="test/!(*.int*).spec.js"
 fi
 
-echo "SPECS    : $SPECS"
-echo "COVERAGE : $COVERAGE"
+echo "┌──────────────┬─────────────────────────"
+echo "| SPECS        │ $SPECS                  "
+echo "│ COVERAGE     │ $COVERAGE               "
+echo "│ CONCURRENCY  │ $CONCURRENCY            "
+echo "│ GLOBAL_SETUP │ $GLOBAL_SETUP           "
+echo "└──────────────┴─────────────────────────"
+
 
 node                             \
     --env-file=./.env            \
     --env-file=./.env.dev        \
-    --experimental-test-coverage \
+    $COVER                       \
     --test-coverage-exclude='packages/testing/**'  \
     --test-coverage-exclude='scripts/reset-test-db.js' \
     --test-coverage-lines=$COVERAGE     \
